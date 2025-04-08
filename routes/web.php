612ff49b38eef4a55ec576acb1dca51d1640b729
Alba Gracia <albagraciaportero@gmail.com>
2025-04-08 21:25:19 +0200

    style changes

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::group(
     function () { //...
 
         Route::get('/', function () {
-            return view('welcome');
+            return view('index');
         });
 
         Route::get('/dashboard', function () {
